03a183311030e0464c69b2e1b4c8f3f4c5d7f42c
Alexander Konovalov <konovalov_a.s@mail.ru>
2021-06-03 15:50:30 +0500

    Добавляет компонеты главной страницы

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Main from '../main/main';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({offersCount}) {
+  return <Main offersCount = {offersCount} />;
 }
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+};
 
 export default App;
